608090210ae2c318138c6fbc7198a72a7544872f
Dwayne Williams <dwaynewillmakeit@gmail.com>
2021-07-11 03:06:27 -0500

    Setting up Server Side Rendering. Used getInitailProp to perform both Server Side Redering and Browser Side render to fetch the Current User

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,47 @@
- const index = () =>{
+ import axios from 'axios'
+
+ const LandingPage = ({currentUser}) =>{
+
+  //The currentUser prop is being retrived from the getInitailProps method
+
+  console.log(currentUser);
+
+
   return  <h1>Landing Page</h1>
 }
 
-export default index;
+LandingPage.getInitialProps = async ({req}) =>{
+
+  //
+  if(typeof window === 'undefined')
+  {
+    console.log(req.headers)
+    //We are on the SERVER
+    //Request shoul be to http:ingress-nginx.......
+    //http://SERVICENAME.NAMESPACE.svc.cluster.local
+    try{
+      const {data} = await axios
+                    .get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',{
+                      headers:req.headers
+                    });
+                    
+
+                    return data;
+    }catch(err){
+      console.log(err.toJSON());
+
+      return {};
+    }
+    
+
+  }else{
+    //We are on the Browser
+    const {data} = await axios.get('/api/users/currentuser');
+   
+    return data;
+
+  }
+
+}
+
+export default LandingPage;
